c90109b6ddb8b0f63cbefcaab75a76fa6eeb5fe8
TheColaber <72760579+TheColaber@users.noreply.github.com>
2021-07-07 15:29:19 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# create-sa-addon
+
+Quick and easy way to setup a folder for your new Scratch Addons addon!
+
+```bash
+npx create-sa-addon
+```
+
+## Developing
+
+```bash
+npm i # Install dependencies
+npm i -g # Globalize package
+```
---
+++ cli.js
@@ -0,0 +1,87 @@
+#! /usr/bin/env node
+
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const question = (question) =>
+  new Promise((resolve) => readline.question(question, resolve));
+
+const fs = require("fs");
+
+(async () => {
+  let name = await question("Name: ");
+  let id = await question("ID: ");
+  let description = await question("Description: ");
+  let username = await question("Username: ");
+  let tags = await question("Tags (Seperate with commas): ");
+  let userscriptMatch = await question(
+    "Userscript Matches (Seperate with commas, Leave blank for no userscript): "
+  );
+  let userstyleMatch = await question(
+    "Userstyle Matches (Seperate with commas, Leave blank for no userscript): "
+  );
+  readline.close();
+
+  let dir = process.cwd() + "\\addons\\" + id;
+
+  if (fs.existsSync(dir)) {
+    if (fs.readdirSync(dir).length) {
+      console.log(
+        "\x1b[31m%s",
+        "[Error] A folder with this addon ID already exists!",
+        "\x1b[0m"
+      );
+      return;
+    }
+  } else fs.mkdirSync(dir);
+
+  const addonJSON = {
+    name,
+    description,
+    credits: [
+      {
+        name: username,
+      },
+    ],
+    tags: tags.split(/,\s/),
+  };
+  if (userscriptMatch) {
+    addonJSON.userscripts = [
+      {
+        matches: userscriptMatch.split(/,\s/),
+        url: "userscript.js",
+      },
+    ];
+
+    const userscriptJS = `export default async function ({ addon, msg, global, console }) {\n\n}\n`;
+    fs.writeFileSync(dir + "\\userscript.js", userscriptJS);
+  }
+  if (userstyleMatch) {
+    addonJSON.userstyles = [
+      {
+        matches: userstyleMatch.split(/,\s/),
+        url: "userstyle.css",
+      },
+    ];
+    fs.writeFileSync(dir + "\\userstyle.css", "");
+  }
+
+  fs.writeFileSync(
+    dir + "\\addon.json",
+    JSON.stringify(addonJSON, null, "  ") + "\n"
+  );
+
+  const addonsDir = process.cwd() + "\\addons\\addons.json";
+  let addonsJSON = fs.readFileSync(addonsDir, "utf8");
+  const substringIndex = addonsJSON.indexOf("// NEW ADDONS ABOVE THIS ↑↑") - 5;
+  addonsJSON =
+    addonsJSON.substring(0, substringIndex) +
+    `  "${id}",\n` +
+    addonsJSON.substring(substringIndex, addonsJSON.length);
+
+  fs.writeFileSync(addonsDir, addonsJSON);
+
+  console.log("\x1b[34m%s", "Addon created:", dir, "\x1b[0m");
+})();
